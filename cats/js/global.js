c1712236e802797e7a95d537e57fa9ebc2ae54ab
Александр Кукулин <119128540+Kykylin88@users.noreply.github.com>
2023-04-18 19:36:44 +0400

    Add files via upload

---
+++ cats/js/global.js
@@ -0,0 +1,46 @@
+const box = document.querySelector(".container");
+const addBtn = document.querySelector(".add-btn");
+const mdBox = document.querySelector(".modal-container");
+const mdClose = mdBox.querySelector(".modal-close");
+const addForm = document.forms.add;
+
+/* 
+    Вс е хранится как текст (в виде строки)
+    localStorage
+        .getItem(name);
+        .setItem(name, "value")
+*/
+
+let user = localStorage.getItem("cats"); // имя пользователя
+if (!user) {
+    user = prompt("Ваше уникальное имя: ", "kykylin88");
+    localStorage.setItem("cats", user);
+}
+
+const path = `https://cats.petiteweb.dev/api/single/${user}`;
+// const path = `https://cats.petiteweb.dev/api/single/lk12`;
+
+/*
+    1)
+        При загрузке страницы проверять, есть ли коты в LS?
+        Если коты уже были в LS - отрисовывать сайт с ними
+        Иначе - отправлять сетевой запрос и просить данные с котами (у БД)
+*/
+let cats = localStorage.getItem("cats-data") // массив с котами
+
+
+
+// let a = 2;
+// let b = a;
+// b = 6;
+// console.log("a", a, ", b", b);
+
+// let c = {a: 2, b: 3}
+// let d = {...c, c: 5, a: 4}; // деструктуризация (копирование объекта или массивва)
+// c.a = 6;
+// console.log("c", c, ", d", d);
+
+
+// 4БДЗ + ГП => отл
+// 4БДЗ (2+) => хор
+// 3БДЗ (1+) => уд
